cd0c5c05feae63b164791531ac411359de3f0fbc
Suparada Saitalae <ann.suparada@gmail.com>
2019-08-06 16:48:33 -0500

    render name

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,15 +1,46 @@
 import React from 'react';
+import ReactDom from 'react-dom'
+import axios from 'axios'
+import { Card, Icon, Image } from 'semantic-ui-react'
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      githubCard: ''
+    }
+  }
+
+  componentDidMount() {
+    this.fetchUsers();
+    
+  }
+
+  fetchUsers = () => {
+    axios(`https://api.github.com/users/annsuparada`)
+      .then(response => {
+        console.log('fetchUsers',response)
+        this.setState({ githubCard: response.data })
+      })
+      
+      .catch(err => {
+        console.log(err)
+      })
+      
+  }
+
+  render() {
+    return (
+      <div className="App">
         <h1>Github User Card</h1>
-      </header>
-    </div>
-  );
+        <h2>{this.state.githubCard.name}</h2>
+          {/* <image key={this.githubCard} src={this.githubCard.avatar_url} />   */}
+      </div>
+    );
+  }
+  
 }
 
 export default App;
